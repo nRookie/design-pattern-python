d27deddda346fecd718a1a05cf34501b644c0184
nrookie <228611676@qq.com>
2020-06-23 07:43:19 +0800

    fix coding spec of pizza

--- python/pizza.py
+++ python/pizza.py
@@ -1,7 +1,7 @@
 import abc
 
-class SimplePizzaFactory:
 
+class SimplePizzaFactory:
 
     def __init__(self, type):
         self.pizza = None
@@ -12,30 +12,32 @@ class SimplePizzaFactory:
         elif type == "clam":
             self.pizza = ClamPizza()
         elif type == "veggie":
-            self.pizza = VeggiePizza();
-
+            self.pizza = VeggiePizza()
 
 
 class PizzaStore:
     __metaclass__ = abc.ABCMeta
+
     def __init__(self, factory):
         pass
-        #self.factory = factory
+        # self.factory = factory
 
     @abc.abstractmethod
     def createPizza(self, type):
         pass
+
     def orderPizza(self, type):
         self.pizza = None
-        #Notice that we've replaced the new operator with a create method
-        #No more concrete instantiation here!
+        # Notice that we've replaced the new operator with a create method
+        # No more concrete instantiation here!
         pizza = self.createPizza(type)
-        #pizza = self.factory.createPizza(type)
+        # pizza = self.factory.createPizza(type)
         pizza.prepare()
         pizza.bake()
         pizza.cut()
         pizza.box()
         return pizza
+
 
 class NYPizzaStore(PizzaStore):
 
@@ -48,16 +50,15 @@ class NYPizzaStore(PizzaStore):
         elif item == "pepperoni":
             return NYStylePepperoniPizza()
         elif item == "clam":
-            return  NYStyleClamPizza()
+            return NYStyleClamPizza()
         elif item == "veggie":
-            return  NYStyleVeggiePizza()
+            return NYStyleVeggiePizza()
         else:
             return None
 
 
 class Pizza:
     __metaclass__ = abc.ABCMeta
-
 
     def __init__(self):
         self.name = None
@@ -66,35 +67,40 @@ class Pizza:
         self.toppings = []
 
     def prepare(self):
-        print("preparing %s"%(self.name))
+        print("preparing %s" % (self.name))
         print("Tossing dough...")
         print("Adding sauce")
         print("Adding toppings: ")
 
         for topping in self.toppings:
-            print("  %s"%(topping))
+            print("  %s" % (topping))
 
     def bake(self):
         print("Bake for 25 minutes at 350")
+
     def cut(self):
         print("Cutting the pizza into diagonal slices")
+
     def box(self):
         print(" Place pizza in official PizzaStore box")
+
     def getName(self):
         return self.name
 
+
 class NYStyleCheesePizza(Pizza):
     def __init__(self):
-        super(NYStyleCheesePizza,self).__init__()
+        super(NYStyleCheesePizza, self).__init__()
         self.name = "NY Style Sauce and Cheese Pizza"
         self.dough = "Thin crust dough"
         self.sauce = "Marinara Sauce"
 
         self.toppings.append("Grated Reggiano Cheese")
 
+
 class ChicagoStyleCheesePizza(Pizza):
     def __init__(self):
-        super(ChicagoStyleCheesePizza,self).__init__()
+        super(ChicagoStyleCheesePizza, self).__init__()
         self.name = "Chicago Style Deep Dish Cheese Pizza"
         self.dough = "Extra Thick Crust Dough"
         self.sauce = "Plum Tomato Sauce"
@@ -105,11 +111,8 @@ class ChicagoStyleCheesePizza(Pizza):
         print("Cutting the pizza into square slices")
 
 
-
-
-
 if __name__ == "__main__":
     nyStore = NYPizzaStore()
 
     pizza = nyStore.orderPizza("cheese")
-    print("Ethan ordered a %s \n"%(pizza.getName()))
+    print("Ethan ordered a %s \n" % (pizza.getName()))
