f66125eb2af2cf67530119f1b61c6294d9f5a6c2
nrookie <228611676@qq.com>
2020-06-24 07:38:46 +0800

    reduce the cost of using synchronization

--- java/Singleton.java
+++ java/Singleton.java
@@ -1,14 +1,17 @@
 public class Singleton {
-    private static Singleton uniqueInstance;
+    private static Singleton uniqueInstance = new Singleton();
 
     private Singleton() {}
 
     public static synchronized Singleton getInstance(){
-        if(uniqueInstance == null) {
-            uniqueInstance = new Singleton();
-        }
         return uniqueInstance;
     }
 
     // other useful methods here
 }
+
+/*
+Using this approach we rely on the JVM to create the unique instance of the Singleton when the 
+class is loaded. The JVM guarantees that the instance will be created before any thread accesses
+the static uniqueInstance variable.
+*/
