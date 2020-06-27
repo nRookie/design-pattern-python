a42df50484e6c3fa7b5fa49762ab567bb1d77e24
kestrel <228611676@qq.com>
2020-06-27 03:42:08 -0400

    add Open garage door command

--- cpp/Command.cpp
+++ cpp/Command.cpp
@@ -38,6 +38,28 @@ public:
   }
 };
 
+class GarageDoor{
+public:
+  GarageDoor(){}
+  void up() {cout<<"Garage Door up!"<<endl;}
+  void down() {cout<<"Garage Door down !" << endl;}
+  void stop() { cout<<"Garage Door stop !" << endl;}
+  void lightOn() {cout<<"Garage Door Light on"<<endl;}
+  void lightOff() { cout<<"Garage Door Light off"<<endl;}
+};
+class GarageDoorOpenCommand : public Command{
+public:
+  GarageDoor * garageDoor;
+
+  GarageDoorOpenCommand(GarageDoor * garageDoor)
+  {
+    this->garageDoor = garageDoor;
+  }
+
+  void execute() {
+    this->garageDoor->up();
+  }
+};
 
 class SimpleRemoteControl{
 private:
@@ -64,7 +86,11 @@ int main()
   Light * light = new Light();
 
   LightOnCommand * lightOn = new LightOnCommand(light);
+  GarageDoor * garageDoor = new GarageDoor();
+  GarageDoorOpenCommand * garageOpen = new GarageDoorOpenCommand(garageDoor);
 
   remote->setCommand(lightOn);
+  remote->buttonWasPressed();
+  remote->setCommand(garageOpen);
   remote->buttonWasPressed();
 }
